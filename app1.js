2152f0d61d3a48b62ecd3e556b5d1700cbdd16af
oscar <oheredia2@ucol.mx>
2021-05-26 14:52:41 -0500

    Correcion del random a enteros

--- app1.js
+++ app1.js
@@ -64,7 +64,7 @@ for(let i=1;i<=300;i++)
     let msg = "Ciclo es: "+i;
     if(np <= 39)
     {
-        let nuevo = new Trabajo(Math.random()*11+4);
+        let nuevo = new Trabajo(Math.floor(Math.random()*11+4));
         cola.agregar(nuevo);
         msg+=" Se creó procceso "+ nuevo.ciclos;
     }
@@ -72,11 +72,12 @@ for(let i=1;i<=300;i++)
     if(cola.ver()!=null)
     {
         cola.ver().ciclos--;
-
+        msg += " Atendiendo "+cola.ver().ciclos;
         if(cola.ver().ciclos==0)
         {
             cola.extraer();
+            msg += " Salió un procceso";
         }
     }
-
+    console.log(msg);
 }
